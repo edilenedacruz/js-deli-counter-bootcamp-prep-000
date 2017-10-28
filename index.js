70dd9e429fca4ae7a9a43c6e5e139ffb5ba7e387
Edilene Da Cruz <hello@edilene.io>
2017-10-28 18:52:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeli = [];
 
 function takeANumber(katzDeli, customer) {
   katzDeli.push(customer)
-  return ("Welcome, `${customer}`. You are number ``") 
+  return (`Welcome, ${customer}. You are number $(katzDeli.length) in line.`); 
 }
