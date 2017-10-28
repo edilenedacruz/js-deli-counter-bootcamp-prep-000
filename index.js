162473c42ca250663188730ff8bc6e7df2f432ef
Edilene Da Cruz <hello@edilene.io>
2017-10-28 19:06:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function takeANumber(katzDeli, customer) {
 }
 
 function nowServing(katzDeli) {
-  // let i = 0;
-  while (var i = 0; i < katzDeli.length) { i++; }
+  let i = 0;
+  while (i < katzDeli.length) { i++; }
   if (katzDeli.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
