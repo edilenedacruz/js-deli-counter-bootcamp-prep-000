1163e65e26801ff53d2c80c73571220c05a0a594
Edilene Da Cruz <hello@edilene.io>
2017-10-28 18:57:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var katzDeli = [];
 
 function takeANumber(katzDeli, customer) {
-  katzDeli.push(customer)
-  return (`Welcome, ${customer}. You are number ${katzDeli.length} in line.`); 
+  katzDeli.push(customer);
+  return (`Welcome, ${customer}. You are number ${katzDeli.length} in line.`);
+}
+
+function nowServing(katzDeli) {
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    
+  }
+
 }
