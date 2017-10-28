7f512b4d229b73d9be77113d955de8622f7caec6
Edilene Da Cruz <hello@edilene.io>
2017-10-28 19:32:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(katzDeli) {
   var line = [];
   let i = 0;
   while (i < katzDeli.length) {
-    line.push(` `+[i+1]+`.` + katzDeli[i])
+    line.push(` `+[i+1]+`. ` + katzDeli[i])
     i++;
   }
     if (katzDeli.length === 0) {
